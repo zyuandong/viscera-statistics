58f54c2b1b09a6d0c5e1da2730a447dd52bb16e2
zyuandong <de1719@163.com>
2019-05-25 16:40:15 +0800

    upgrade spleen

--- src/components/spleen.vue
+++ src/components/spleen.vue
@@ -1,28 +1,194 @@
 <template>
   <div id="spleen">
-    <router-link to="/menu">返回</router-link>
     <div class="clearfix">
       <div class="pull-left width-4 p-10 block-d">
-        <div class="clearfix">
+        <router-link to="/menu">
           <div class="title-box">脾部数据</div>
-        </div>
-        <div class="main-box">
-          <ChartPin></ChartPin>
+        </router-link>
+
+        <div class="main-img-box">
+          <img src="./../assets/images/spleen/spleen.png" alt="">
         </div>
 
         <div class="sub-title">
           <span class="main-text">脾部数据</span><span class="sub-text">分析</span>
           <span class="sub-title-line w-6"></span>
         </div>
+
+        <div class="clearfix">
+          <div class="pull-left" style="width:45%">
+            <ChartPie
+              :id="'pie1'"
+              :width=300
+              :height=300
+              :data="pie1"
+            ></ChartPie>
+          </div>
+          <div class="pull-left" style="width:55%"></div>
+        </div>
       </div>
+
       <div class="pull-left width-6 p-10 block-c">
         <div class="sub-title">
           <span class="main-text">脾梗数据</span><span class="sub-text">统计</span>
           <span class="sub-title-line w-6"></span>
         </div>
 
+        <div class="clearfix">
+          <div class="pull-left" style="width:20%">
+            <ChartBar
+              :id="'bar1'"
+              :width=300
+              :height=300
+              :data="bar1"
+            ></ChartBar>
+          </div>
+          <div class="pull-left" style="width:35%">
+            <ChartBar
+              :id="'bar2'"
+              :width=300
+              :height=300
+              :data="bar2"
+            ></ChartBar>
+          </div>
+          <div class="pull-left" style="width:15%">
+            <ChartBar
+              :id="'bar3'"
+              :width=300
+              :height=300
+              :data="bar3"
+            ></ChartBar>
+          </div>
+          <div class="pull-left" style="width:30%">
+            <div class="pull-left" style="width:50%">
+              <ChartPie
+                :id="'pie2'"
+                :width=100
+                :height=100
+                :data="pie2"
+              ></ChartPie>
+            </div>
+            <div class="pull-left" style="width:50%">
+              <ChartPie
+                :id="'pie3'"
+                :width=100
+                :height=100
+                :data="pie3"
+              ></ChartPie>
+            </div>
+            <div class="pull-left" style="width:50%">
+              <ChartPie
+                :id="'pie4'"
+                :width=100
+                :height=100
+                :data="pie4"
+              ></ChartPie>
+            </div>
+            <div class="pull-left" style="width:50%">
+              <ChartPie
+                :id="'pie5'"
+                :width=100
+                :height=100
+                :data="pie5"
+              ></ChartPie>
+            </div>
+          </div>
+        </div>
 
+        <div class="clearfix">
+          <div class="pull-left" style="width:50%">
+            <ChartBar
+              :id="'bar4'"
+              :width=300
+              :height=300
+              :data="bar4"
+            ></ChartBar>
+          </div>
+          <div class="pull-left" style="width:50%">
+            <ChartBar
+              :id="'bar5'"
+              :width=300
+              :height=300
+              :data="bar5"
+            ></ChartBar>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  import Axios from 'axios';
+  import ChartPie from './charts/chartPie.vue';
+  import ChartBar from './charts/chartBar.vue';
+
+  export default {
+    data() {
+      return {
+        pie1: {},
+        bar1: {},
+        bar2: {},
+        bar3: {},
+        bar4: {},
+        bar5: {},
+        pie2: {},
+        pie3: {},
+        pie4: {},
+        pie5: {},
+      }
+    },
+    components: {
+      ChartPie,
+      ChartBar,
+    },
+    created() {
+      Axios.get('./data/spleen/pie1.json')
+        .then(resp => {
+          this.pie1 = resp.data;
+        });
+
+      Axios.get('./data/spleen/bar1.json')
+        .then(resp => {
+          this.bar1 = resp.data;
+        });
+
+      Axios.get('./data/spleen/bar2.json')
+        .then(resp => {
+          this.bar2 = resp.data;
+        });
+      Axios.get('./data/spleen/bar3.json')
+        .then(resp => {
+          this.bar3 = resp.data;
+        });
+      Axios.get('./data/spleen/bar4.json')
+        .then(resp => {
+          this.bar4 = resp.data;
+        });
+      Axios.get('./data/spleen/bar5.json')
+        .then(resp => {
+          this.bar5 = resp.data;
+        });
+
+      Axios.get('./data/spleen/pie2.json')
+        .then(resp => {
+          this.pie2 = resp.data;
+        });
+
+      Axios.get('./data/spleen/pie3.json')
+        .then(resp => {
+          this.pie3 = resp.data;
+        });
+
+      Axios.get('./data/spleen/pie4.json')
+        .then(resp => {
+          this.pie4 = resp.data;
+        });
+
+      Axios.get('./data/spleen/pie5.json')
+        .then(resp => {
+          this.pie5 = resp.data;
+        });
+    }
+  }
+</script>
